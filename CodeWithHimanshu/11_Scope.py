7b6334a55c9a8f01a9d8167196015d62707559c9
himanshu <himanshuvkarm1996@gmail.com>
2021-09-28 01:36:31 +0530

    New Learnings addedd

---
+++ CodeWithHimanshu/11_Scope.py
@@ -0,0 +1,30 @@
+sameNameVar = 22
+
+
+def getvar():
+    # sameNameVar=45  we cannot assign the value in the same scope before global dec.
+    #    print(sameNameVar)  If we try to use the variable before global dec. It gives an error.
+    global sameNameVar
+    print(sameNameVar)
+
+
+getvar()
+
+
+def nestedfunction():
+    sameNameVar1 = 34
+
+    def nestedfunc2():
+        global sameNameVar1
+        sameNameVar1 = 44
+
+    print("before", sameNameVar1)
+    nestedfunc2()
+    print("after", sameNameVar1)
+    """Even if the sameNameVar1 is made global it wont affect the value in local scope of parent function. Because 
+    global keyword only looks for the varable in global scope. If it does not find any, it creates one with the same
+    name used after the keyword. If that variable is printed in the global scope it holds the same value defined."""
+
+
+nestedfunction()
+print(sameNameVar1)  # Prints the value defined in nestedFunc2
